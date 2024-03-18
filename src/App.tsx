8883306b3347e00fe888b26c4af5f7e0362cc4dd
Luan-Neumann-Dev <luan.neumann.dev@gmail.com>
2024-03-18 20:49:07 -0300

    Upando o projeto do GitHubFinder

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom'
+
+//css module
+import classes from './App.module.css'
+
+function App() {
+  return (
+    <div className={classes.app}>
+      <h1>GitHub Finder</h1>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default App
